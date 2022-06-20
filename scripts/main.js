62aadfb619c89ae9855ff0979fc2dc8d7150e5e6
NimVrod <hdec0759@gmail.com>
2022-06-20 20:26:44 +0200

    Typed
    Added "typed" animation, changed the about description a bit.

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,14 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
 
+let options = {
+    "strings" : ["NimVrod","a Web developer", "a Python scripter", "a Student"]
+    , "typeSpeed" : 100
+    , "loop" : true
+}
+
+let typed = new Typed(".typed", options);
+
 $(document).ready(function (){
     $(document).scroll(function () {
         let scroll = $(document).scrollTop();
